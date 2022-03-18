d6cd043c062959a2a577b37f70c9774e70f2f76f
Kefat <85555513+Kefat@users.noreply.github.com>
2022-03-18 10:20:30 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -63,7 +63,7 @@ let randomCount = $.isNode() ? 20 : 5;
         $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
 
         if ($.isNode()) {
-          await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+          // await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
         }
         continue
       }
